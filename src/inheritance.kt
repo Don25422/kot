11101edea1d33be28c047783190e2c76ca8f62f4
Don25422 <danglaskimathi5@gmail.com>
2025-03-27 12:23:56 +0300

    fourth commit

---
+++ src/inheritance.kt
@@ -0,0 +1,35 @@
+//Parent class/Super class/Base class
+open class Animal{
+    var age = 3
+    var gender = "Male"
+
+    fun makesound(){
+        println("Animal is speaking")
+    }
+}
+
+//Child class/Derived class/Sub class
+class dog:Animal(){
+    fun bark(){
+        println("Woof! Woof!")
+    }
+}
+
+class cat{
+    var color = "White"
+    var hasfur = true
+
+    fun meow(){
+        println("Meow! Meow!")
+    }
+}
+
+fun main() {
+    var a = Animal()
+
+    var d = dog()
+    d.bark()
+    println(d.gender)
+
+    var c = cat()
+}
